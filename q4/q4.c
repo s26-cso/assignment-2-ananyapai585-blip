ddf71f4ee2a54ee07c634c27342bce4863ae21a2
ananyapai585-blip <ananyapai585@gmail.com>
2026-04-12 00:53:41 +0530

    q4 done some fixes left

--- q4/q4.c
+++ q4/q4.c
@@ -1,7 +1,49 @@
 # include <stdio.h>
+# include <dlfcn.h> //  the dynamic linking library 
+# include <string.h>
+
+
 int main(){
-    char op[50];
-    scanf("%s",op);
-    long long int n,m;
-    scanf("%lld %lld",&n,&m);
+    
+
+    char lib_n[6]="./lib";
+    char so[4]=".so";
+
+    int(*operation)(int, int); // function pointer to a function that takes 2 ints as input 
+                               // returns int as output
+
+    while(1){
+    char lib[50]="";
+    char op[10];
+    scanf("%s",op); // operation type input by user 
+    if(strcmp(op,"exit")==0)
+    {
+         break;
+    } // dummy exit prompt: user prompt 
+    int n,m;
+    scanf("%d %d",&n,&m); // integer inputs by user
+    
+    strcat(lib,lib_n);
+    strcat(lib,op);
+    strcat(lib,so); // lib="./lib<op>.so" ./ symbolises that it is in the current working directory
+
+    void * ptr = dlopen(lib,RTLD_LAZY); // loads only that library that is required 
+     
+    // if no such operation-file exists 
+    if (ptr == NULL) {
+    printf("Such an operation does not exist!\n");
+    continue;
+    }
+
+    operation=dlsym(ptr,op); // finds the function with the name <op>, operation contains addr of the 
+                             // req function 
+    
+
+    printf("%d\n",operation(n,m)); // function call 
+
+    dlclose(ptr);
+    }
+    // NOTE : function ptrs vs functions
+
+    // Prompt to stop the calculator app ? 
 }
